025f00b5d96f2e87063a62f881886039fa188418
alexandrova-ma <peurakallo@gmail.com>
2019-03-14 05:04:41 -0400

    commit

--- form/enter.php
+++ form/enter.php
@@ -10,13 +10,13 @@ $admin = 'admin';
 $pass = '5f4dcc3b5aa765d61d8327deb882cf99';
 
 if($_POST['submit']){
- if($admin == $_POST['user'] AND $pass == md5($_POST['pass'])){
- $_SESSION['admin'] = $admin;
- header("Location: index.php");
- exit;
- }
+    if($admin == $_POST['user'] AND $pass == md5($_POST['pass'])){
+        $_SESSION['admin'] = $admin;
+        header("Location: index.php");
+        exit;
+    }
     else {
-    echo '<p>Логин или пароль неверны!</p>';
+        echo '<p>Логин или пароль неверны!</p>';
     }
 }
 ?>
@@ -31,8 +31,8 @@ if($_POST['submit']){
 </head>
 <body>
 <form method="post">
- Username: <input type="text" name="user" /><br />
- Password: <input type="password" name="pass" /><br />
- <input type="submit" name="submit" value="Войти" />
+    Username: <input type="text" name="user" /><br />
+    Password: <input type="password" name="pass" /><br />
+    <input type="submit" name="submit" value="Войти" />
 </body>
 </html>
